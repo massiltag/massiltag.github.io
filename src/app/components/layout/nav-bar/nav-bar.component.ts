c90dc38b9d5f24c47f0af652348353edb899c6d5
Massil Taguemout <massiltag@icloud.com>
2022-02-23 01:02:10 +0100

    update github actions workflow

--- src/app/components/layout/nav-bar/nav-bar.component.ts
+++ src/app/components/layout/nav-bar/nav-bar.component.ts
@@ -11,16 +11,6 @@ export class NavBarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    /*var prevScrollpos = window.pageYOffset;
-    window.onscroll = function() {
-      var currentScrollPos = window.pageYOffset;
-      if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "0";
-      } else {
-        document.getElementById("navbar").style.top = "-70px";
-      }
-      prevScrollpos = currentScrollPos;
-    }*/
   }
 
   focus(event): void {
